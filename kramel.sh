8f151591f72075a162ffa86a636612147878e69a
Robin <hanya8493@gmail.com>
2023-05-25 01:50:43 +0000

    Kramel: nuke lld

--- kramel.sh
+++ kramel.sh
@@ -107,7 +107,6 @@ elif [[ "${COMPILER}" = clang ]]; then
 	O=out
         CROSS_COMPILE=aarch64-linux-gnu-
         CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-	LD=ld.lld
 	AR=llvm-ar
 	AS=llvm-as
 	NM=llvm-nm
